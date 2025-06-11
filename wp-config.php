9d62326d251eaab4bfd3a465c9f23aa64b64531b
Ian McAllister <iemcallister@gmail.com>
2025-06-11 10:02:33 -0700

    Fixing wp-config error

--- wp-config.php
+++ wp-config.php
@@ -92,7 +92,7 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', transliterator_create_from_rules );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 define( 'WP_DEBUG_LOG', true );
